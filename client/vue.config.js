d8b9c446d47d4c1736ade91dc652ff0bab7af8a7
Nicholas.Arocho <nick@boldlyfine.com>
2020-05-18 17:46:34 -0700

    initial commit: through steps 1 & 2

---
+++ client/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devServer: {
+    port: 8080,
+    proxy: 'http://localhost:5500'
+  },
+  lintOnSave: false,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          loader: 'graphql-tag/loader'
+        }
+      ]
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/assets/css/variables.scss";`
+      }
+    }
+  }
+}
